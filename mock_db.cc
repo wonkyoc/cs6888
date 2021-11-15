9f49e42b80eca7f20beb4aad950f529db39af7c9
Akash <akash7nair@gmail.com>
2021-11-15 12:33:40 -0500

    add test cases, write function login wrapper, rename login to database names

--- mock_db.cc
+++ mock_db.cc
@@ -10,16 +10,50 @@ using ::testing::_; // Matcher for parameters
 class MockDB : public DataBaseConnect {
 public:
     MOCK_METHOD0(fetchRecord, int());
-    MOCK_METHOD1(logout, bool(string uname));
-    MOCK_METHOD2(login, bool(string uname, string passwd));
-    MOCK_METHOD2(login2, bool(string uname, string passwd));
+    MOCK_METHOD1(redis, bool(string uname));
+    MOCK_METHOD2(aeroSpike, bool(string uname, string passwd));
+    MOCK_METHOD2(mysql, bool(string uname, string passwd));
 };
 
-TEST(MyDBTest, LoginTest) {
+TEST(MyDBTest0, LoginTest) {
+    //case 1
     MockDB mdb;
-    MyDatabase db(mdb);
-    ON_CALL(mdb, login(_,_)).WillByDefault(Return(true));
-    int retValue = 1;
-    EXPECT_EQ(retValue, 1);
+    MyDatabase* x = new MyDatabase (mdb);
+    EXPECT_CALL(mdb,redis("x")).WillOnce(Return(true));
+    EXPECT_EQ(x->loginWrapper(), true);
+
+}
+
+
+TEST(MyDBTest1, LoginTest) {
+    //case 2
+    MockDB mdb;
+    MyDatabase* x = new MyDatabase (mdb);
+    EXPECT_CALL(mdb,redis("x")).WillOnce(Return(false));
+    EXPECT_CALL(mdb,aeroSpike("x","y")).WillOnce(Return(true));
+    EXPECT_EQ(x->loginWrapper(), false);
+
+}
+
+
+TEST(MyDBTest2, LoginTest) {
+    //case 3
+    MockDB mdb;
+    MyDatabase* x = new MyDatabase (mdb);
+    EXPECT_CALL(mdb,redis("x")).WillOnce(Return(false));
+    EXPECT_CALL(mdb,aeroSpike("x","y")).WillOnce(Return(false));
+    EXPECT_CALL(mdb,mysql("x","y")).WillOnce(Return(true));
+    EXPECT_EQ(x->loginWrapper(), true);
+}
+
+
+TEST(MyDBTest3, LoginTest) {
+    //case 4
+    MockDB mdb;
+    MyDatabase* x = new MyDatabase (mdb);
+    EXPECT_CALL(mdb,redis("x")).WillOnce(Return(false));
+    EXPECT_CALL(mdb,aeroSpike("x","y")).WillOnce(Return(false));
+    EXPECT_CALL(mdb,mysql("x","y")).WillOnce(Return(false));
+    EXPECT_EQ(x->loginWrapper(), false);
 
 }
